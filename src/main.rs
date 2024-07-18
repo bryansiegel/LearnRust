5fc2d1d916f7185e1c0d13dec6472f1a2b57a53d
Bryan Siegel <bryansiegel@gmail.com>
2024-07-17 19:08:34 -0700

    tuple

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
 fn main() {
-    println!("Hello, world!");
+    // let x = 5;
+    // let x = x + 1;
+    // {
+    //     let x = x * 2;
+    //     println!("The value of x in the inner scope is: {x}");
+    // }
+    //
+    // println!("The value of x is: {x}");
+    //
+    // let f: bool = false;
+    // let t = true;
+    // println!("{} {t}", f);
+
+    // let tup: (i32, f64, u8) = (500, 6.4, 1);
+    //
+    // println!("{:?}", tup);
+
+let x: (i32, f64, u8) = (500, 6.4, 1);
+
+    let five_hundred = x.0;
+
+    let six_point_four = x.1;
+
+    let one = x.2;
+
+    println!("{one}");
+    println!("{five_hundred}");
+    println!("{six_point_four}");
+
 }
